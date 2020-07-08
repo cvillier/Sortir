f8c5661cc81e9b22150700561c271d8c37ab9087
antonycochet <cochetantony@gmail.com>
2020-07-08 11:48:01 +0200

    Fix , page accueil = se connecter

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/avoir", name="home")
      */
     public function home () {
         return $this->render("main/home.html.twig");
