13072af333ed653e2fac894a1264b08fdf9835bb
Corentin Villiermet <corentin.villiermet@gmail.com>
2020-07-08 12:39:09 +0200

    mise en forme "Créer sortie" twig et FormType

--- src/Form/CreationSortieFormType.php
+++ src/Form/CreationSortieFormType.php
@@ -10,6 +10,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +22,10 @@ class CreationSortieFormType extends AbstractType
     {
         $builder
             ->add('nom',TextType::class, [
-                'label'  => 'Nom de la sortie : '
+                'label'  => 'Nom de la sortie : ',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
             ])
 
             ->add('datedebut', DateTimeType::class, [
@@ -32,10 +37,38 @@ class CreationSortieFormType extends AbstractType
                 ]
             ])
 
-            ->add('datecloture')
-            ->add('nbinscriptionsmax')
-            ->add('duree')
-            ->add('descriptioninfos')
+            ->add('datecloture', DateTimeType::class, [
+                'label'  => 'Date limite d inscription : ',
+                "widget" => 'single_text',
+                "data" => new \DateTime(),
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('nbinscriptionsmax', IntegerType::class, [
+                'label'  => 'Nombre de places : ',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('duree', IntegerType::class, [
+                'label'  => 'Durée : ',
+                "data" => 90,
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('descriptioninfos', TextType::class, [
+                'label'  => 'Description et infos : ',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            //à supprimer : on ne demande pas au User de choisir son campus : on affiche le campus auquel il est affilié
             ->add("campus", EntityType::class, [
                 'class' => Campus::class,
                 'choice_label' => 'nom_campus',
@@ -46,6 +79,7 @@ class CreationSortieFormType extends AbstractType
                 'choice_label' => 'nom_ville',
                 'mapped' => false
             ])
+
             ->add("noLieu", EntityType::class, [
                 'class' => Lieux::class,
                 'choice_label' => 'nom_lieu',
