7205b4cc21355777332a8742dd1d9cb1b11a1158
Corentin Villiermet <corentin.villiermet@gmail.com>
2020-07-09 14:12:31 +0200

    amélioration de modif profil

--- src/Form/EditAccountType.php
+++ src/Form/EditAccountType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,40 +18,17 @@ class EditAccountType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
-//            ->add('roles')
-            ->add('roles', ChoiceType::class, [
-                'choices' => [
-                    'Admin' => 'ROLE_ADMIN',
-                    'User' => 'ROLE_USER',
-                    'Organisateur' => 'ROLE_ORGANISATEUR',
-                    'Participant' => 'ROLE_PARTICIPANT',
-                ],
-                'expanded'  => false, // liste déroulante
-                'multiple'  => true, // choix multiple
-            ])
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('email')
-            ->add('actif')
+
             ->add('pseudo', TextType::class, [
                 'label' => 'Pseudo : '
             ])
 
-//            ->add('roles')
-
-            ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe : '
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom : '
             ])
 
             ->add('nom', TextType::class, [
                 'label' => 'Nom : '
-            ])
-
-            ->add('prenom', TextType::class, [
-                'label' => 'Prénom : '
             ])
 
             ->add('telephone', IntegerType::class, [
@@ -56,12 +39,30 @@ class EditAccountType extends AbstractType
                 'label' => 'Email'
             ])
 
-            ->add('campus')
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe : '
+            ])
 
-//            ->add('actif')
+//            ajouter confirmation password
+
+            ->add('campus')
 
             ->add('photo', FileType::class, [
                 'label' => 'Ma photo : '
+            ])
+
+//            ->add('actif')
+
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'User' => 'ROLE_USER',
+                    'Organisateur' => 'ROLE_ORGANISATEUR',
+                    'Participant' => 'ROLE_PARTICIPANT',
+                ],
+                'expanded'  => false, // liste déroulante
+                'multiple'  => true, // choix multiple
+                'label' => 'Le rôle de cet utilisateur : '
             ]);
     }
 
